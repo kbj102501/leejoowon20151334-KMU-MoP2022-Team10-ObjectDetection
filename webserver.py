478ccb7370c473882863d788240a575e687e9fd1
BeomJun <kbj102501@naver.com>
2022-12-07 01:50:11 +0900

    이미지 불러오는 방식 수정

--- webserver.py
+++ webserver.py
@@ -1,3 +1,4 @@
+import cgi
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import socketserver
 import urllib.parse as urlparse
@@ -20,85 +21,39 @@ with open("./item.json", 'rt', encoding="UTF-8") as f:
     items = json.loads(f.read())
 
 class myHandler(BaseHTTPRequestHandler):
-  # GET형식의 파라미터를 파싱한다.(url의 물음표(?) 이후의 값을 딕셔너리로 파싱한다.   
-    
-  def __get_Parameter(self, key):
-    # 해당 클래스에 __param변수가 선언되었는지 확인한다.
-    if hasattr(self,"_myHandler__param") == False:
-      if "?" in self.path:
-        # url의 ?이후의 값을 파싱한다.
-        self.__param = dict(urlparse.parse_qsl(self.path.split("?")[1], True))
-      else :
-        # url의 ?가 없으면 빈 딕셔너리를 넣는다.
-        self.__param = {}
-    if key in self.__param:
-      return self.__param[key]
-    return None
-  # POST형식의 form 데이터를 파싱한다.
-  def __get_Post_Parameter(self, key):
-    # 해당 클래스에 __post_param변수가 선언되었는지 확인한다.
-    if hasattr(self,"_myHandler__post_param") == False:
-      # 해더로 부터 formdata를 가져온다.
-      data = self.rfile.read(int(self.headers['Content-Length']))
-      if data is not None:
-        self.__post_param = dict(urlparse.parse_qs(data.decode()))
-      else :
-        self.__post_param = {}
-    if key in self.__post_param:
-      return self.__post_param[key][0]
-    return None
- 
-  # http 프로토콜의 header 내용을 넣는다.
-  def __set_Header(self, code):
-    # 응답 코드를 파라미터로 받아 응답한다.
-    self.send_response(code)
-    self.send_header('Content-type','text/html')
+  def do_POST(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'image/jpeg')
     self.end_headers()
- 
-  # http 프로토콜의 body내용을 넣는다.
-  def __set_Body(self, data):
-    # body 응답은 byte형식으로 넣어야 한다.(필요에 의해 주석 해제)
-    #response = io.BytesIO()
-    #response.write(b"<html><body><form method='post'><input type='text' name='test' value='hello'><input type='submit'></form></body></html>")
-    #self.wfile.write(response.getvalue())
-    # data(string)를 byte형식으로 변환해서 응답한다.
-    self.wfile.write(data.encode())
- 
-  # POST 형식의 request일 때 호출된다.
-  def do_GET(self):
-    # GET 방식의 파라미터의 data 값을 취득한다.
-    data = str(self.__get_Parameter('data')).replace(" ","+")
-    
-    # response(응답)할 body내용이다.
-    body = f"{data}"
-    if(data != None and data not in "None"):
-        global model
-        global items
-        img = io.BytesIO(base64.b64decode(data))
-        img = np.array(Image.open(img))
-        img = mmcv.imread(img)
+
+    ctype, pdict = cgi.parse_header(
+        self.headers['content-type'])
+
+    pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+
+    if ctype == 'multipart/form-data':
+        fields = cgi.parse_multipart(self.rfile, pdict)
+        messagecontent = fields.get('image')
+        # print(fields)
+        print(type(messagecontent))
+        img = Image.open(io.BytesIO(messagecontent[0]))
+        img.save("./test.jpg")
+        img = mmcv.imread('./test.jpg')
         result = inference_detector(model, img)
-        ret = []
+        ret = {}
         for (i, res) in zip(range(len(result)), result):
             if len(res) > 0:
                 for arr in res:
                     if arr[-1] > 0.4:
-                        ret.append(items[model.CLASSES[i]])
-        body = f'{"{"} "status":"success","data":{ret} {"}"}'
-         
-        
-        # response header code는 200(정상)으로 응답한다.
-        self.__set_Header(200)
-    # response body는 위 html 코드 내용을 넣는다.
-    self.__set_Body(body)
-  # POST 형식의 request일 때 호출된다.
-  def do_POST(self):
-    # response header code는 200(정상)으로 응답한다.
-    self.__set_Header(200)
-    # response body는 form으로 받은 test의 값으로 응답한다.
-    self.__set_Body(self.__get_Post_Parameter('test'))
+                      if items[model.CLASSES[i]] not in ret.keys():
+                          ret[items[model.CLASSES[i]]] = 1
+                      else:
+                          ret[items[model.CLASSES[i]]] += 1
+                    print(items[model.CLASSES[i]], end=": ")
+                    print(arr[-1])
+        print(ret)
+        body = f'{ret}'
+        self.wfile.write(body.encode())
  
-# http server를 생성한다.
 httpd = HTTPServer(('', 8004), myHandler)
-# 서버 중지(Ctrl + Break)가 나올때까지 message 루프를 돌린다.
 httpd.serve_forever()
